ac50090e6d1141943f5ea6d9a62c15cc056c966e
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-03-27 12:17:32 -0300

    ``` Refactoriza la gestión de estilos y optimiza la estructura de componentes: mejora la legibilidad del código y ajusta la interfaz de usuario. ```

--- music-house-frontend/src/components/utils/ArrowBack.jsx
+++ music-house-frontend/src/components/utils/ArrowBack.jsx
@@ -17,10 +17,10 @@ const ArrowBack = () => {
       onClick={handleBackClick}
       sx={{
         position: 'fixed', // 👉 Esto la deja flotante
-        top:200,
+        top: 200,
         left: 16,
         zIndex: 9999, // 👈 Asegura que quede por encima de todo
-       padding:"1px",
+        padding: '1px',
         display: 'flex',
         margin: 1,
         alignItems: 'center',
@@ -31,18 +31,19 @@ const ArrowBack = () => {
         boxShadow: 'var(--box-shadow)',
         transition: 'transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out',
         animation: 'pulse 1.5s infinite ease-in-out',
-       
+
         '&:hover': {
           transform: 'scale(1.1)', // Efecto de crecimiento suave
           boxShadow: 'var(--box-shadow)'
         }
       }}
     >
-      <ArrowBackIcon sx={{ 
-        color: 'var(--color-secundario)',
-         fontSize: '20px' ,
-       
-         }} />
+      <ArrowBackIcon
+        sx={{
+          color: 'var(--color-secundario)',
+          fontSize: '20px'
+        }}
+      />
     </Box>
   )
 }
